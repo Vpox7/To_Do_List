ae7d9a7279eae7b565f188b6827b7e4c7dcfaba8
Vpox7 <vpoxclip1@gmail.com>
2025-03-27 20:20:33 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int main() {
+	cout << "********************TO DO LIST OPTIONS***********************" << endl;
+	cout << "1)Add the task" << endl;
+	cout << "2)Remove the task" << endl;
+	cout << "3)Display the task" << endl;
+	cout << "4)Edits as Done" << endl;
+	cout << "5)Marks as done " << endl;
+	cout << "6)Exit" << endl;
+
+	return 0;
+}
